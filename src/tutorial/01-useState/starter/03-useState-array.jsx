6a549b403c276a8a4978c61aa0e8ab5f625c95ed
toan <bachmatoan2910@gmail.com>
2023-06-20 11:53:03 -0400

    UseState Array - Complete

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,31 @@
-const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+import {people} from "../../../data"
+import {useState} from "react"
+function UseStateArray(){
+  
+  const [peopleList, setList] = useState(people);
+  function removeItem(id) {
+    setList((prevPeople) => {
+      return prevPeople.filter((person) => {
+        return person.id !== id;
+      });
+    });
+    console.log(peopleList);
+  }
+  return (
+    <div>
+      {
+        peopleList.map((person) => {
+          const {id, name} = person;
+          return (
+            <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+          );
+        })
+      }
+    </div>
+  );
 };
 
 export default UseStateArray;
