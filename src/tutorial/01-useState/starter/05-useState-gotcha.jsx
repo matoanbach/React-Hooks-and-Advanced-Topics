2e0af9ee1de0438d3229950df80bb0420d525138
toan <bachmatoan2910@gmail.com>
2023-06-20 12:54:08 -0400

    UseState - SetTimeout Example

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,22 @@
+import {useState} from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+  function handleClick() {
+    setTimeout(() => {
+      console.log('clicked the button');
+      setValue((currentState) => {
+        const newState = currentState + 1
+        return newState;
+      }); 
+    }, 3000);
+  }
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button type="button" className="btn" onClick={handleClick}>increase</button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
